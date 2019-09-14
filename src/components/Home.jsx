4fd41fd3e9b6006e26fb2a39bd508d995fe8f600
Josh Parry <josh.parry97@gmail.com>
2019-09-14 17:48:35 +1000

    created homepage component

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Welcome to the home page</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
+
+
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 
 import BootNav from './components/BootNav';
 import Item from './components/Item';
+import Home from './components/Home';
 
 
 const Routes = (
   <Router>
     <BootNav />
     <div>
+      <Route exact path="/" component={ Home } />
       <Route exact path='/item' component={Item}/>
     </div>
   </Router>
