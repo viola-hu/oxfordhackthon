1a368fdfd3ed2c1597f1655ad5461d05fee8e964
Viola Hu <lovelyviolaat@hotmail.com>
2019-09-15 13:04:54 +1000

    Updates

---
+++ precache-manifest.a60032d7e67990e62e81aff0f9f9ecc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80d40d49281c52f34bef5a4a07323fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81d5757651d4b2b5571a",
+    "url": "/static/css/main.3b0031c6.chunk.css"
+  },
+  {
+    "revision": "ec3f30aea2f4f584db44",
+    "url": "/static/js/2.c918cf06.chunk.js"
+  },
+  {
+    "revision": "81d5757651d4b2b5571a",
+    "url": "/static/js/main.cc2aa76c.chunk.js"
+  },
+  {
+    "revision": "a63919e6d8cd3aa25982",
+    "url": "/static/js/runtime~main.9baa0e95.js"
+  },
+  {
+    "revision": "0cbd653de0f4f0e9f7c779c947bfef4e",
+    "url": "/static/media/leftLightening.0cbd653d.png"
+  }
+]);
